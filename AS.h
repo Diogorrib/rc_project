a27a36cf3023072d3640d4583331da2268400486
Diogorrib <119450853+Diogorrib@users.noreply.github.com>
2023-11-14 22:30:20 +0000

    Add files via upload

---
+++ AS.h
@@ -0,0 +1,8 @@
+#ifndef __AS_H__
+#define __AS_H__
+
+#include "utils.h"
+
+void filter_input(int argc, char **argv);
+
+#endif // __AS_H__
---
+++ user.h
@@ -0,0 +1,8 @@
+#ifndef __USER_H__
+#define __USER_H__
+
+#include "utils.h"
+
+void filter_input(int argc, char **argv);
+
+#endif // __USER_H__
