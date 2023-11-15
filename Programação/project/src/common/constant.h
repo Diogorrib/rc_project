abd142d633f227b23fc0f5297985b300916a64a2
GustavoCostaHenriques <119442854+GustavoCostaHenriques@users.noreply.github.com>
2023-11-15 18:38:06 +0000

    Add files via upload

---
+++ Programação/project/src/common/constant.h
@@ -0,0 +1,7 @@
+#ifndef __CONSTANT_H__
+#define __CONSTANT_H__
+
+#define DEFAULT_PORT "58001"    // 58000 + GN
+#define DEFAULT_IP "localhost"
+
+#endif // __CONSTANT_H__
---
+++ Programação/project/src/server/server.h
@@ -0,0 +1,9 @@
+#ifndef __SERVER_H__
+#define __SERVER_H__
+
+#include "../common/utils.h"
+#include "../common/constant.h"
+
+void filter_input(int argc, char **argv);
+
+#endif // __SERVER_H__
